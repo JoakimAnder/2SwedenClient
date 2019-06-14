8606bf23eb341b76a430390652807ada1fb7ac26
Joakim Andersson <Joakim.Ander@Hotmail.com>
2019-06-14 12:07:41 +0200

    added production url

--- src/tools/DAO.js
+++ src/tools/DAO.js
@@ -1,7 +1,7 @@
 const decodePolyline = require('decode-google-map-polyline');
 
 const baseURL = (process.env.NODE_ENV === "production")
-    ? "http://localhost:5000/"
+    ? "https://to-sweden-jocke.herokuapp.com/"
     : "http://localhost:5000/"
 
 
